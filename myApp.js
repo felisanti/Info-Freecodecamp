85204eeb8fc7c668cfa23308f8c7ca68be4df32f
felisanti <santifel22@gmail.com>
2024-01-18 18:45:59 -0500

    add hsts

--- myApp.js
+++ myApp.js
@@ -15,6 +15,9 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
 
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 
 
 
